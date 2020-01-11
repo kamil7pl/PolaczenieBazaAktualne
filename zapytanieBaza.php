5b8a09cc9c6493d6eb2d53e0ff85b8049b47d7bf
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-01-11 01:48:01 +0100

    Add files via upload

---
+++ zapytanieBaza.php
@@ -0,0 +1,26 @@
+<?php
+class polaczenieBaza
+{
+  private $host="localhost";
+  private $dbName= "testowa";
+  private $user= "root";
+  private $pass= "";
+  protected $con;
+  
+  public function __construct()
+  {
+    $this->con=new mysqli($this->host, $this->user, $this->pass, $this->dbName); 
+    
+    if (mysqli_connect_error())  
+    { 
+      die('Błąd połączenia.'); 
+    } 
+  
+  
+    echo "Połączono z bazą. Użyto konstruktora. <br />"; 
+    $this->con->query("insert into podzielnaprzez values(null,74,99,1505)");
+    $this->con->close(); 
+  }
+}
+$spr = new polaczenieBaza();
+?>
