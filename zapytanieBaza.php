1e7083cf832d5462191cd8cf2e51d9d934120fd2
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-01-18 21:48:28 +0100

    Update zapytanieBaza.php

--- zapytanieBaza.php
+++ zapytanieBaza.php
@@ -1,26 +1,12 @@
 <?php
-class zapytanieBaza
-{
-  private $host="localhost";
-  private $dbName= "testowa";
-  private $user= "root";
-  private $pass= "";
-  protected $con;
-  
-  public function __construct()
-  {
-    $this->con=new mysqli($this->host, $this->user, $this->pass, $this->dbName); 
-    
-    if (mysqli_connect_error())  
-    { 
-      die('Błąd połączenia.'); 
-    } 
-  
-  
-    echo "Połączono z bazą. Użyto konstruktora. <br />"; 
-    $this->con->query("insert into podzielnaprzez values(null,74,99,1505)");
-    $this->con->close(); 
-  }
+require_once("polaczenieBaza.php");
+class zapytanieBaza extends polaczenieBaza{
+		public function zapytanie(){
+			$this->con->query("insert into podzielnaprzez values(null,74100024,99,1505)");
+			$this->con->close();
+			echo 'Wstawiono rekord.';
+		}
 }
-$spr = new zapytanieBaza();
+$sprawdzenie=new zapytanieBaza();
+$sprawdzenie->zapytanie();
 ?>
