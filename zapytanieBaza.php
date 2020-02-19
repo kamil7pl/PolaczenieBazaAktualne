686f7d9936d6e413c5074f538341cfbadb9fdadb
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-02-19 15:42:37 +0100

    Update zapytanieBaza.php

--- zapytanieBaza.php
+++ zapytanieBaza.php
@@ -2,7 +2,7 @@
 require_once("polaczenieBaza.php");
 class zapytanieBaza extends polaczenieBaza{
 		public function zapytanie(){
-			$this->con->query("insert into podzielnaprzez values(null,74100024,99,1505)");
+			$this->con->query("insert into podzielnaprzez values(null,74100024,99,1505)");//Jak będzie tekst zamiast liczb to używać apostrofów.
 			$this->con->close();
 			echo 'Wstawiono rekord.';
 		}
